4ebc65640f5955c1fff143d1184bdadf55902c03
robertoneris <rnerisn@correo.uss.cl>
2022-10-19 17:27:30 -0300

    updat

---
+++ sustentaweb/src/Component/hoja2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function hoja2() {
+  return (
+    <div>hoja2</div>
+  )
+}
--- sustentaweb/src/app.jsx
+++ sustentaweb/src/app.jsx
@@ -1,41 +1,12 @@
 import React, {Fragment} from "react";
 import css from "./css/app.css"
+import  {hoja1}  from "./Component/hoja1";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export function App(){
+export function App()  {
+    
     return(
-        
-         <Fragment>
-            <div>CORREO ELECTRONICO</div>
-            <input type = "text" placeholder = "ingrese el correo electronico empresarial"/>
-            <div>NOMBRE Y APELLIDO</div>
-            <input type = "text" placeholder = "ingrese nombre y apellido del representante"/>
-            <div>NOMBRE DE LA ORGANIZACIÓN</div>
-            <input type = "text" placeholder = "ingrese el nombre de la organizacion"/>
-            <div>ÁREA EN LA ORGANIZACIÓN</div>
-            <select name="select" >
-                <option value="1">seleccione el ara de organizacion de su empresa</option>
-            </select>
-            <div>TELÉFONO DE CONTACTO</div>
-            <input type = "text" placeholder = "ingrese el telefono de contacto de la empresa"/>
-            <div>AÑO DE FORMALIZACIÓN DEL NEGOCIO</div>
-            <input type = "text" placeholder = "ingrese el año de formalizacion de la empresa"/>
-            <div> NÚMERO DE IDENTIFICADOR FISCAL DE LA ORGANIZACIÓN</div>
-            <input type = "text" placeholder = "ingrese el RUT de la empresa"/>
-            <div>PAIS</div>
-            <div>REGIÓN DEL PAÍS</div>
-            <div>COMUNA</div>
-            <div> MONTO DE FACTURACIÓN DEL ÚLTIMO AÑO FISCAL</div>
-            <input type = "text" placeholder = "ingrese el monto de factuzacion del ultimo año"/>
-            <div>SITIO WEB DE LA ORGANIZACIÓN </div>
-            <input type = "text" placeholder = "ingrese el sitio web de la empresa"/>
-            <div>RRSS DE LA ORGANIZACIÓN </div>
-             
-            <button class="button2" type = "button" >
-            testing</button>
-   
-
-         </Fragment>    
-        
-
+    hoja1()   
     )
-}
+  }
