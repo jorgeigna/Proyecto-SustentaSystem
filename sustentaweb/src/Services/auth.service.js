b27243243e47c3fcd07ae7b6b5334dab03d527fb
robertoneris <rnerisn@correo.uss.cl>
2022-12-01 16:56:19 -0300

    comentado authservice

--- sustentaweb/src/Services/auth.service.js
+++ sustentaweb/src/Services/auth.service.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8082/api/auth/";
 class AuthService {
+  //conexion con el sistema login de usuario mediante promesas axio
   login(Correo, Contraseña) {
     return axios
       .post(API_URL + "signin", {
@@ -23,6 +24,7 @@ class AuthService {
       Contraseña,
     });
   }
+  //conexion para registrar la tabla de caracteristicas del cliente en la base mongo mediante promesas axio
   registercarac(
     Correo,
     Apellido,
@@ -80,6 +82,7 @@ class AuthService {
       Lvl,
     });
   }
+  //conexion con el sistema de registro de respuestas en la base mongo mediante promesas axio
   registresp(
     Correo,
     resp1,
@@ -107,13 +110,15 @@ class AuthService {
       resp10,
     });
   }
+  //funcion de cierre de sesion en la cual se elimina la sesion de la localstorage
   logout() {
     localStorage.removeItem("user");
   }
+  //funcion que obtiene el usuario de la sesion actual iniciada
   getCurrentUser() {
     return JSON.parse(localStorage.getItem("user"));
   }
-
+// funcion encargada de enviar el correo para el caso de menor nivel de dominio de sustentabilidad
   sendmail(mensaje) {
     return axios.post(API_URL + "ems", {
       mensaje,
