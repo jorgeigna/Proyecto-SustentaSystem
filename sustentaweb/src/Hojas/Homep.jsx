15c0a1a3b8392f7a3fa6cc558f92920576d4e519
robertoneris <rnerisn@correo.uss.cl>
2022-12-01 18:07:24 -0300

    corregido css de imagenes de la pagina principal

--- sustentaweb/src/Hojas/Homep.jsx
+++ sustentaweb/src/Hojas/Homep.jsx
@@ -76,61 +76,67 @@ class Homep extends Component {
           <strong style={{ fontSize: "30pt" }}>nuestros clientes</strong>
           <div className="row">
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen10-300x55.jpg"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen5.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen2.png"
                 alt=""
               />
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen2.png"
-                alt=""
-              />
+
               <img
                 className="imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2022/01/qhantatisustentable-300x296.png"
                 alt=""
               />
+                            <img className = "imgcl"
+                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/z-Destileria-Carupamp.png"
+                alt=""
+              />
+                            <img className = "imgcl"
+                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Yarna-shop.png"
+                alt=""
+              />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen3.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen7.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen6.jpg"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen4.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Imagen8.jpg"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/z-KKO-Real.png"
                 alt=""
               />
+              
             </div>
 
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2022/01/Celeritech--300x78.jpg"
                 alt=""
               />
@@ -139,196 +145,185 @@ class Homep extends Component {
                 src="http://kairosgestionintegral.com/wp-content/uploads/2022/01/Moneda-a-Granel-300x300.jpg"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/biotech-1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/caraota-digital-300x72.png"
                 alt=""
               />
+                            <img className = "imgcl"
+                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Garzon-300x138.png"
+                alt=""
+              />
+              <img className = "imgcl"
+                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/savake-300x130.png"
+                alt=""
+              />
+
+
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/yarabeth-1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Mundo-PC-1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/2m-ingenieria-1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/aserca-1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/amand-300x61.png"
                 alt=""
               />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Sondel-1.png"
                 alt=""
               />
 
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/salvador-300x83.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/vivax.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/abloy.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Banco-Caribe.png"
                 alt=""
               />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/behrens-pharma.png"
                 alt=""
               />
 
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/avabsalud.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/beco-viejo.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/cavececo-CLIENTE-300x58.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/blasam.png"
                 alt=""
               />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/centigom-300x136.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Datanalisis-300x78.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/kenco.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/LOGO-EMPLEATE-CLIENTE.png"
+                alt=""
+              />
+                            <img className = "imgcl"
+                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/QuickPress.png"
                 alt=""
               />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Mercantinl.png"
                 alt=""
               />
-              <img
+
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/morales-morales-asociados-300x60.png"
                 alt=""
               />
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/morales-morales-asociados-300x60.png"
-                alt=""
-              />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Rica-Internacional.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/vital-care.png"
                 alt=""
               />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/sun-300x55.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/ultimas-noticias.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/sanitas-300x139.gif"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/cadena-capriles-1.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/nsm-grupo-300x120.png"
                 alt=""
               />
             </div>
             <div className="column">
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/nodus.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/z-the-VIP.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/ferretotal-300x138.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Bimbo.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/z-Automercado-Plazas.png"
                 alt=""
               />
-              <img
+              <img className = "imgcl"
                 src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/alpina.png"
                 alt=""
               />
-            </div>
-            <div className="column">
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Garzon-300x138.png"
-                alt=""
-              />
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/savake-300x130.png"
-                alt=""
-              />
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/Yarna-shop.png"
-                alt=""
-              />
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/QuickPress.png"
-                alt=""
-              />
-              <img
-                src="http://kairosgestionintegral.com/wp-content/uploads/2021/12/z-Destileria-Carupamp.png"
-                alt=""
-              />
+            <br />
+            <br />
             </div>
 
-            <br />
-            <br />
           </div>
         </form>
         <br />
